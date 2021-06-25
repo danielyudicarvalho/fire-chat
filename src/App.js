0a7ecaf24790addc5c9910827337c63542a9efaa
danielyudicarvalho <danielyudicarvalho@gmail.com>
2021-06-25 12:27:17 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -116,7 +116,7 @@ function ChatMessage(props){
   return(
     <>
       <div className={`message ${messageClass}`}>
-        <img src={photoURL ||'https://api.adorable.io/avatars/23/abott@adorable.png' }/>
+        <img src={photoUR}/>
         <p>{text}</p>
       </div>
     </>
